7bb7be219a861c93d43f7df33391cd4730641a88
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-27 13:03:22 +0100

    feat: rest apis, clients and cors errors

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const app = express();
 
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
