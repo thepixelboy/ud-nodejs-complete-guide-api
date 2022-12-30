d0d8172d7c5bde2d610158ac2e0b6d78c0c28420
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-30 11:21:42 +0100

    feat: fetch lists of posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,8 +2,14 @@ exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
+        _id: "1",
         title: "First Post",
         content: "This is the first post!",
+        imageUrl: "images/dummy-image.png",
+        creator: {
+          name: "Maximilian",
+        },
+        createdAt: new Date(),
       },
     ],
   });
