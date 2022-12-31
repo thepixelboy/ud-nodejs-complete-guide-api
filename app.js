d1f2579f4dc57a0210417d0e3097aa82028add9e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-31 14:29:19 +0100

    feat: static images and error handling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -8,6 +9,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(bodyParser.json()); // application/json
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -20,6 +22,14 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+  next();
+});
 
 mongoose.set("strictQuery", true);
 mongoose
