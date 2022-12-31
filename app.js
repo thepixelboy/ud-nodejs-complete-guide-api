9bc177102a0b264f656a004dc35ad0f9e1b6f837
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-31 13:32:50 +0100

    feat: setup a post model

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const feedRoutes = require("./routes/feed");
+
+require("dotenv").config();
 
 const app = express();
 
@@ -18,4 +21,16 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    dbName: "messages",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
