eb8ae0f93e7b41fdeab501c5799581b81754c85b
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-01-02 16:01:45 +0100

    feat: add a user model

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const multer = require("multer");
+
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 require("dotenv").config();
 
@@ -46,6 +48,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
---
+++ routes/auth.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+router.put("/signup", (req, res, next) => {});
+
+module.exports = router;
