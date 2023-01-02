a113ca26b8a76aeff15977e32e9fe86c2138a9ca
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-01-02 13:06:07 +0100

    feat: update posts

--- routes/feed.js
+++ routes/feed.js
@@ -5,7 +5,7 @@ const feedController = require("../controllers/feed");
 const router = express.Router();
 
 router.get("/posts", feedController.getPosts);
-
+router.get("/post/:postId", feedController.getPost);
 router.post(
   "/post",
   [
@@ -14,7 +14,13 @@ router.post(
   ],
   feedController.createPost
 );
-
-router.get("/post/:postId", feedController.getPost);
+router.put(
+  "/post/:postId",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.updatePost
+);
 
 module.exports = router;
