e23ba5da7eec77945b13c615a0d8b41300a2a645
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-01-03 20:00:46 +0100

    feat: assignment 7

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { body } = require("express-validator");
 const User = require("../models/user");
 const authController = require("../controllers/auth");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
@@ -26,5 +27,12 @@ router.put(
 );
 
 router.post("/login", authController.login);
+router.get("/status", isAuth, authController.getUserStatus);
+router.patch(
+  "/status",
+  isAuth,
+  [body("status").trim().not().isEmpty()],
+  authController.updateUserStatus
+);
 
 module.exports = router;
