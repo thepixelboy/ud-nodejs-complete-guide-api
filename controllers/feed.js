5356a82dff3663c75d24d060715b823642cceed8
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-01-03 19:16:08 +0100

    feat: connect posts and users

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 const { validationResult } = require("express-validator");
+
 const Post = require("../models/post");
+const User = require("../models/user");
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -13,6 +15,7 @@ exports.getPosts = (req, res, next) => {
     .then((count) => {
       totalItems = count;
       return Post.find()
+        .populate("creator", "name")
         .skip((currentPage - 1) * perPage)
         .limit(perPage);
     })
@@ -47,19 +50,28 @@ exports.createPost = (req, res, next) => {
   const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
+  let creator;
   const post = new Post({
     title: title,
     content: content,
     imageUrl: imageUrl,
-    creator: { name: "Maximilian" },
+    creator: req.userId,
   });
   post
     .save()
     .then((result) => {
-      console.log(result);
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then((result) => {
       res.status(201).json({
         message: "Post created successfully!",
-        post: result,
+        post: post,
+        creator: { _id: creator._id, name: creator.name },
       });
     })
     .catch((err) => {
