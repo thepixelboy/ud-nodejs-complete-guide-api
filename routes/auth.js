e956fadee2f5e641e9b8cf61e8660dcf209f416f
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-01-03 13:12:52 +0100

    feat: add user signup validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const { body } = require("express-validator");
+const User = require("../models/user");
+const authController = require("../controllers/auth");
+
 const router = express.Router();
 
-router.put("/signup", (req, res, next) => {});
+router.put(
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject("E-mail address already exists!");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password").trim().isLength({ min: 5 }),
+    body("name").trim().not().isEmpty(),
+  ],
+  authController.signup
+);
 
 module.exports = router;
