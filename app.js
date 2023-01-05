532958d971afc63d490f8cec319be32c16e936c7
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-01-05 11:48:45 +0100

    feat: share the io instance accross files

--- app.js
+++ app.js
@@ -68,11 +68,9 @@ mongoose
   })
   .then((result) => {
     const server = app.listen(8080);
-    const io = require("socket.io")(server, {
-      cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-      },
+    const io = require("./socket").init(server);
+    io.on("connection", (socket) => {
+      console.log("Client connected");
     });
   })
   .catch((err) => {
